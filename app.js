a32fada485540e92d7d5a033a8979c557c876b56
Saurav Gharti <sp.gharti@gmail.com>
2016-04-06 18:17:03 +0545

    created the custom app server

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ db.once('open', function () {
 var dashboard = require('./routes/dashboard');
 
 var app = express();
+var http = require('http').createServer(app);
+
+var io = require('socket.io')(http);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -49,29 +52,23 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
+  res.end(err.message);
+});
+
+// Socket From here
+io.on('connection', function(socket){
+  socket.on('test', function (data){
+    socket.emit('test', data);
   });
 });
 
 
-module.exports = app;
+
+http.listen(3000,'localhost', function(){
+  console.log('listening on http://localhost:' + 3000+'/');
+});
+
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var app = require('../app')
+
+
 router.get('/', function (req, res, next){
 	res.sendFile('/templates/dashboard.html', {root: __dirname.substring(0, __dirname.indexOf('routes'))});
 });
+
 
 module.exports = router;
